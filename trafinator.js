9e399d6520cf366a7d7ed8e9d08ee5304d557958
GulbyLT <gulbinasrok@gmail.com>
2025-10-28 18:59:30 +0200

    add instructions and email address preview for double-checking the receiver

--- trafinator.js
+++ trafinator.js
@@ -1,14 +1,10 @@
 var dropdown = document.getElementById("vendor");
-
-
-
 var button = document.getElementById("emailButton");
+var emailPreview = document.getElementById("emailPreview");
 
-//temp filling for entry field
-
-
-
-
+dropdown.addEventListener("change", function() {
+    emailPreview.textContent = `To: ${dropdown.value}`;
+});
 
 button.addEventListener("click", function() {
     var carNumber = document.getElementById("car-number").value.toString().toUpperCase();
@@ -18,9 +14,5 @@ button.addEventListener("click", function() {
 
 function openEmailWithRecipient(recipient, carNumber) {
     var subject = encodeURIComponent("Puuttuva rekisteröinti autolle " + carNumber);
-    //var bodyText = encodeURIComponent("Hei, %0A%0A Huomasimme että auton " + carNumber + " rekisteröinti meille puuttuu. Voitteko rekisteröidä auton mahdollisimman pian meille, jotta voimme viedä rahoituksen loppuun. Kiitos jo etukäteen.");
-    
-
-    //var mailtoLink = "mailto:" + recipient + "?subject=" + subject + "&body=" + body;
     window.location.href = `mailto:${recipient}?subject=${subject}&body=Hei,%0D%0A%0D%0AHuomasimme että auton ${carNumber} rekisteröinti meille puuttuu. Voitteko rekisteröidä auton mahdollisimman pian meille, jotta voimme viedä rahoituksen loppuun.%0D%0A%0D%0AKiitos jo etukäteen. %0D%0A%0D%0AYstävällisin terveisin %0D%0ADanske Finance %0D%0AAsiakaspalvelu`;
 }
