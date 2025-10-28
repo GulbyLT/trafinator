b6f21ebb572327acb05185489e90e46f4720848d
GulbyLT <gulbinasrok@gmail.com>
2025-10-28 12:32:17 +0200

    first testing version to be checked in work laptop

--- trafinator.js
+++ trafinator.js
@@ -4,6 +4,7 @@ var button = document.getElementById("emailButton");
 
 var recipient = dropdown.value
 
+
 button.addEventListener("click", function() {
     console.log("Email button clicked");
     console.log("vendor selected: " + dropdown.value);
@@ -11,8 +12,9 @@ button.addEventListener("click", function() {
 });
 
 function openEmailWithRecipient(recipient) {
+    var sender = "your_email@example.com";
     var subject = encodeURIComponent("Test Email");
     var body = encodeURIComponent("TEST EMAIL BODY");
-    var mailtoLink = "mailto:" + recipient + "?subject=" + subject + "&body=" + body;
+    var mailtoLink = "mailto:" + sender + "?to=" + recipient + "&subject=" + subject + "&body=" + body;
     window.location.href = mailtoLink;
 }
