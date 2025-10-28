b1d45e0150cc724cc33369e96643101b29fa5ebe
GulbyLT <gulbinasrok@gmail.com>
2025-10-28 09:40:01 +0200

    fix email receiver not choosing correctly

--- trafinator.js
+++ trafinator.js
@@ -2,8 +2,17 @@ var dropdown = document.getElementById("vendor");
 
 var button = document.getElementById("emailButton");
 
+var recipient = dropdown.value
 
 button.addEventListener("click", function() {
     console.log("Email button clicked");
     console.log("vendor selected: " + dropdown.value);
+    openEmailWithRecipient(dropdown.value);
 });
+
+function openEmailWithRecipient(recipient) {
+    var subject = encodeURIComponent("Test Email");
+    var body = encodeURIComponent("TEST EMAIL BODY");
+    var mailtoLink = "mailto:" + recipient + "?subject=" + subject + "&body=" + body;
+    window.location.href = mailtoLink;
+}
