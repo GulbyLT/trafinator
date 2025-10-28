5ede601381ec20c54d57406db0536b69cc288a91
GulbyLT <gulbinasrok@gmail.com>
2025-10-28 18:43:01 +0200

    fix line breaks not appearing properly in email client

--- trafinator.js
+++ trafinator.js
@@ -1,20 +1,26 @@
 var dropdown = document.getElementById("vendor");
 
+
+
 var button = document.getElementById("emailButton");
 
-var recipient = dropdown.value
+//temp filling for entry field
+
+
+
 
 
 button.addEventListener("click", function() {
-    console.log("Email button clicked");
-    console.log("vendor selected: " + dropdown.value);
-    openEmailWithRecipient(dropdown.value);
+    var carNumber = document.getElementById("car-number").value.toString().toUpperCase();
+    openEmailWithRecipient(dropdown.value, carNumber);
+    
 });
 
-function openEmailWithRecipient(recipient) {
-    var sender = "your_email@example.com";
-    var subject = encodeURIComponent("Test Email");
-    var body = encodeURIComponent("TEST EMAIL BODY");
-    var mailtoLink = "mailto:" + sender + "?to=" + recipient + "&subject=" + subject + "&body=" + body;
-    window.location.href = mailtoLink;
+function openEmailWithRecipient(recipient, carNumber) {
+    var subject = encodeURIComponent("Puuttuva rekisteröinti autolle " + carNumber);
+    //var bodyText = encodeURIComponent("Hei, %0A%0A Huomasimme että auton " + carNumber + " rekisteröinti meille puuttuu. Voitteko rekisteröidä auton mahdollisimman pian meille, jotta voimme viedä rahoituksen loppuun. Kiitos jo etukäteen.");
+    
+
+    //var mailtoLink = "mailto:" + recipient + "?subject=" + subject + "&body=" + body;
+    window.location.href = `mailto:${recipient}?subject=${subject}&body=Hei,%0D%0A%0D%0AHuomasimme että auton ${carNumber} rekisteröinti meille puuttuu. Voitteko rekisteröidä auton mahdollisimman pian meille, jotta voimme viedä rahoituksen loppuun.%0D%0A%0D%0AKiitos jo etukäteen. %0D%0A%0D%0AYstävällisin terveisin %0D%0ADanske Finance %0D%0AAsiakaspalvelu`;
 }
